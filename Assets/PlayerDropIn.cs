735b578b160e0afb875d6b6c904fff615c8ebe58
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-11-06 20:17:37 -0500

    Added the beggining of a start screen with players entering

---
+++ Assets/PlayerDropIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDropIn : MonoBehaviour {
+
+    // Use this for initialization
+    public int PlayerNumber = 1;
+    public bool PlayerIn = false;
+    Image PlayerImage;
+
+	void Start () {
+        PlayerImage = GetComponentInChildren<Image>();
+        PlayerImage.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Controller" + PlayerNumber + "_Dash"))
+        {
+            PlayerIn = true;
+            PlayerImage.gameObject.SetActive(true);
+        }
+	}
+}
---
+++ Assets/screenController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenController : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject newPlayerScreen;
+    public GameObject startScreen;
+
+    public void ChangeToPlayerScreen()
+    {
+        startScreen.SetActive(false);
+        newPlayerScreen.SetActive(true);
+    }
+}
