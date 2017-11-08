a6072f74b2ca1aec7da519abc40e29376cafec84
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-11-07 20:42:36 -0500

    Created the beggining for a directional box

---
+++ Assets/DirectionalBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalBox : MonoBehaviour {
+
+    // Use this for initialization
+    player player;
+
+	void Start () {
+        player = GetComponentInParent<player>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.CurrentlyFacing == player.DirectionFacing.Right)
+        {
+            transform.position = player.transform.position + Vector3.right * 4;
+        }
+        else if (player.CurrentlyFacing == player.DirectionFacing.Left)
+        {
+            transform.position = player.transform.position + Vector3.left * 4;
+        }
+        else if (player.CurrentlyFacing == player.DirectionFacing.Up)
+        {
+            transform.position = player.transform.position + Vector3.up * 4;
+        }
+        else if (player.CurrentlyFacing == player.DirectionFacing.Down)
+        {
+            transform.position = player.transform.position + Vector3.down * 4;
+        }
+    }
+}
